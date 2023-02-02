9bc0f6410507fcefe0f05abf1dab32451a6f3b22
GibiJohn97 <124139672+GibiJohn97@users.noreply.github.com>
2023-02-02 14:55:11 +0530

    Add files via upload

---
+++ student-teacher-crud-master/src/main/java/com/ochibooh/teacher_student_crud/entity/RequestResponse.java
@@ -0,0 +1,20 @@
+package com.ochibooh.teacher_student_crud.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+public class RequestResponse {
+    int responseCode;
+    String message;
+    Object data;
+	public int getResponseCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
