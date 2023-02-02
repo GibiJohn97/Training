84ea255c195b099b4f7ca5698997dbee3e877c1a
GibiJohn97 <124139672+GibiJohn97@users.noreply.github.com>
2023-02-02 16:05:11 +0530

    Add files via upload

---
+++ springboot-kafka-real-world-project/kafka-consumer-database/src/main/java/net/javaguides/springboot/entity/WikimediaData.java
@@ -0,0 +1,25 @@
+package net.javaguides.springboot.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "wikimedia_recentchange")
+@Getter
+@Setter
+public class WikimediaData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String wikiEventData;
+
+	public void setWikiEventData(String eventMessage) {
+		// TODO Auto-generated method stub
+		
+	}
+}
